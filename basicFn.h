6a272dd77f3689319b6dfec050dcf03d5aeb9eeb
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-03-15 01:41:46 +0530

    Exit Functions coded

--- basicFn.h
+++ basicFn.h
@@ -1,8 +1,67 @@
+void gotoxy(int,int);
+void setcolor(int);
+void exitFN(void);
+
 
 
 void ShowMenu(void)
 {
-printf("hello world !!");
+extern int i;
+
+char choice;
+
+
+
+
+SetConsoleTitle("Menu");
+
+    Sleep(10);
+    system("cls");
+
+    gotoxy(10,2);
+    setcolor(6);
+    printf("1 : New Entry..");
+
+    gotoxy(10,4);
+    setcolor(2);
+    printf("2 : See Records");
+
+      gotoxy(10,6);
+    setcolor(3);
+    printf("3 : Delete Records");
+
+       gotoxy(10,8);
+    setcolor(4);
+    printf("4 : Exit");
+
+    fflush(stdin);
+
+    readAgain:
+
+   gotoxy(10,10);
+    choice =getch();
+
+    if(choice == '1')
+    {
+
+    }else if(choice == '1')
+    {
+
+    }else if(choice == '2')
+    {
+
+    }else if(choice == '3')
+    {
+
+    }else if(choice == '4')
+    {
+            exitFN();
+
+    }else
+    {
+        printf("\n");
+        goto readAgain;
+    }
 
 
 }
@@ -38,3 +97,14 @@ void setcolor(int ForgC)
      }
      return;
 }
+
+void exitFN(void)
+{
+    system("cls");
+    setcolor(4);
+    gotoxy(40,12);
+    printf("Thanx For Visiting Us");
+    getch();
+    exit(0);
+}
+
--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include<stdio.h>
 #include<windows.h>
+#include<stdlib.h>
+#include<conio.h>
 
 //user Define Header Files
 #include"basicFn.h" //(showMenu,gotoxy,setcolor)
